4baef84bca83d6a510639a2bd6081e5c0fefcace
Nikita Chernykh <chernykhmain@gmail.com>
2016-02-28 19:10:17 -0500

    Update commit

---
+++ Assets/Scripts/DragHandeler.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragHandeler : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler {
+	public static GameObject itemBeingDragged;
+	Vector3 startPosition;
+
+    public Image bone;
+    public Image progressbar;
+    public bool coolingDown;
+    public float waitTime = 30.0f;
+
+    void Update()
+    {
+        if (coolingDown == true)
+        {
+            //Reduce fill amount over 30 seconds
+         //   cooldown.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        }
+    }
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = gameObject;
+		startPosition = this.transform.position;
+
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		this.transform.position = Input.mousePosition;
+        progressbar.fillAmount += 5.0f / waitTime * Time.deltaTime;
+        bone.fillAmount += 1.0f / waitTime * Time.deltaTime;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = null;
+       
+		//this.transform.position = startPosition;
+	}
+
+    
+
+
+
+	#endregion
+	
+}
---
+++ Assets/Scripts/hover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems; 
+using UnityEngine.UI;
+
+public class hover : MonoBehaviour
+{
+	public GameObject panel;
+	private GameObject Carrot;
+	void Start()
+	{
+
+
+	}
+
+	void update()
+	{
+		
+	}
+
+	void OnMouseOver()
+	{
+		
+		Debug.Log ("you are over the object");
+
+		panel.gameObject.SetActive (true);
+	}
+
+	void OnMouseExit ()
+	{
+		panel.gameObject.SetActive (false);
+	}
+		
+}
